29bfead45e594028ffd4a8ff62e6ae3699570fd9
FLEJOSNE <florian.lejosne@ynov.com>
2021-04-26 17:49:29 +0200

    [ADD] file

--- js/secret.js
+++ js/secret.js
@@ -1,21 +1,12 @@
 const ok = "Secret"
-const URL = "nice.html"
 
 function secret(str) {
     if (str === 'Actinidia') {
-        console.error('Nice dude')
-        console.log("Click here " + ok.link(URL));
+        console.log("Bien vu beaugosse");
     } else {
-        close()
+        window.close()
     }
 }
-
-let win;
-
-function open() {
-    win = window.open("https://x33lys.github.io/secret.html", "_blank", "width=900, height=786");
-}
-
-function close() {
-    win.close();
+function openRequestedPopup() {
+    window.open("https://x33lys.github.io/secret","_blank",'');
 }
